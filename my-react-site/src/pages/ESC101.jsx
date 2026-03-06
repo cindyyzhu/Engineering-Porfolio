57dcba2cd9eed44389a935951f30b7ee8421c10d
Cindy <cindyzhu8207@gmail.com>
2026-03-06 12:58:20 -0500

    edit text

--- my-react-site/src/pages/ESC101.jsx
+++ my-react-site/src/pages/ESC101.jsx
@@ -2,7 +2,7 @@ function ESC101() {
     return (
         <div className="page">
             <h1>ESC101 Backpacks Stands for Commuters</h1>
-            <p>Write your description here. Include objectives, role, and outcomes.</p>
+            <p>[insert text here lol]</p>
             <img src="/images/esc101.jpg" alt="ESC101 Project" />
         </div>
     );
